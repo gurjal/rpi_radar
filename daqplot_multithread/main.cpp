75f8e9006a80890b4af74248f0dd4af0cecd6efd
gurjal <shaanraya135@gmail.com>
2021-02-25 04:02:14 +0000

    modified:   main.cpp

--- daqplot_multithread/main.cpp
+++ daqplot_multithread/main.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <vector>
 #include <thread>
+#include <sstream>
+#include <iostream>
 
 #include <bcm2835.h>
 
@@ -11,21 +13,20 @@
 
 using namespace std;
 
-#define ADC_DATA_SIZE 2048
 #define REAL 0
 #define IMAG 1
 
- // dac variables
-volatile int _hz = 16;
-volatile int _steps = 128;
+#define HZ 16
+#define STEPS 128
+#define PERIODS 16
+#define TUNE 0
 
 // calculate step variables
-volatile int _tune = 0;
-volatile int _step_delay = (1000000 / _steps / _hz) - _tune;
-volatile uint16_t _step_incr = 0xFFFF / _steps;
+volatile int _step_delay = (1000000 / STEPS / HZ) - TUNE;
+volatile uint16_t _step_incr = 0xFFFF / STEPS;
 
 // adc variables
-volatile uint16_t _adc_data[ADC_DATA_SIZE] = {0};
+volatile uint16_t _adc_data[STEPS] = {0};
 volatile char _softSpan_data[] = { 0x00, 0x10 };
 volatile char _send_data[]     = { 0x00, 0x00 };
 volatile char _read_data[]     = { 0x00, 0x00 };
@@ -39,8 +40,16 @@ volatile int _len = 2;
 volatile fftw_complex *_in;
 volatile fftw_complex *_out;
 volatile fftw_plan _p;
-volatile double _fft_mag[ADC_DATA_SIZE];
-volatile double _fft_ang[ADC_DATA_SIZE];
+
+volatile fftw_complex *_in_v[PERIODS];
+volatile fftw_complex *_out_v[PERIODS];
+volatile fftw_plan _p_v[PERIODS];
+
+volatile double _fft_mag[STEPS] = {0};
+volatile double _freespace_fft_mag[STEPS] = {0};
+volatile double _fft_ang[STEPS] = {0};
+
+volatile double _hamming[STEPS] = {0};
 
 inline double mag(double real, double imag) {
   return sqrt((real * real) + (imag * imag));
@@ -86,7 +95,7 @@ int spi_init() {
   //bcm2835_gpio_fsel(RPI_BPLUS_GPIO_J8_10, BCM2835_GPIO_FSEL_OUTP);
   //bcm2835_gpio_fsel(RPI_BPLUS_GPIO_J8_13, BCM2835_GPIO_FSEL_OUTP);
 
-  //bcm2835_gpio_write(RPI_BPLUS_GPIO_J8_08, LOW); //G2
+  //bcm2835_gpio_write(RPI_BPLUS_GPIO_J8_08, HIGH); //G2
   //bcm2835_gpio_write(RPI_BPLUS_GPIO_J8_10, HIGH); //G1
   //bcm2835_gpio_write(RPI_BPLUS_GPIO_J8_13, HIGH); //G0
 
@@ -96,15 +105,27 @@ int spi_init() {
   //bcm2835_gpio_fsel(RPI_BPLUS_GPIO_J8_07, BCM2835_GPIO_FSEL_OUTP);
 
   //bcm2835_gpio_write(RPI_BPLUS_GPIO_J8_03, HIGH); //G2
-  //bcm2835_gpio_write(RPI_BPLUS_GPIO_J8_05, HIGH); //G1
+  //bcm2835_gpio_write(RPI_BPLUS_GPIO_J8_05, LOW); //G1
   //bcm2835_gpio_write(RPI_BPLUS_GPIO_J8_07, HIGH); //G0
 
   // allocating space to fftw vars
-  _in = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * ADC_DATA_SIZE);
-  _out = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * ADC_DATA_SIZE);
+  //_in = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * STEPS);
+  //_out = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * STEPS);
+  for (int i = 0; i < PERIODS; i++) {
+    _in_v[i]  = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * STEPS);
+    _out_v[i] = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * STEPS);
+  }
   
   // create fftw plan
-  _p = fftw_plan_dft_1d(ADC_DATA_SIZE, (fftw_complex*)_in, (fftw_complex*)_out, FFTW_FORWARD, FFTW_ESTIMATE);
+  //_p = fftw_plan_dft_1d(STEPS, (fftw_complex*)_in, (fftw_complex*)_out, FFTW_FORWARD, FFTW_ESTIMATE);
+
+  for (int i = 0; i < PERIODS; i++)
+    _p_v[i] = fftw_plan_dft_1d(STEPS, (fftw_complex*)_in_v[i], (fftw_complex*)_out_v[i], FFTW_FORWARD, FFTW_ESTIMATE);
+
+  for ( int n = 0; n < STEPS; n++ ) {
+    _hamming[n] = (0.54 - (0.46 * cos((double)(2 * 3.14159265 * n)/(double)STEPS)));
+    //printf("%f\n", _hamming[n]);
+  }
 
 
   return 0;
@@ -114,7 +135,7 @@ void run_dac() {
 
   int cur_mag = 0;
 
-  for (int s = 0; s < _steps; s++) {
+  for (int s = 0; s < STEPS; s++) {
     bcm2835_aux_spi_write(cur_mag);
     bcm2835_delayMicroseconds(_step_delay);
     cur_mag += _step_incr;
@@ -124,7 +145,7 @@ void run_dac() {
 
 void run_adc() {
 
-  for (int i = 0; i < _steps; i++) {
+  for (int i = 0; i < STEPS; i++) {
 
     bcm2835_spi_transfernb((char*)_softSpan_data, (char*)_read_data, (int)_len);
     bcm2835_gpio_set(RPI_BPLUS_GPIO_J8_33);
@@ -141,8 +162,8 @@ void run_daq() {
 
   uint16_t cur_mag = 0;
 
-  for (int p = 0; p < _hz; p++, cur_mag = 0) {
-    for (int s = 0; s < _steps; s++) {
+  for (int p = 0; p < PERIODS; p++, cur_mag = 0) {
+    for (int s = 0; s < STEPS; s++) {
 
       // dac write
       bcm2835_aux_spi_write(cur_mag);
@@ -152,9 +173,9 @@ void run_daq() {
       bcm2835_gpio_set(RPI_BPLUS_GPIO_J8_33);
       bcm2835_gpio_clr(RPI_BPLUS_GPIO_J8_33);
       bcm2835_spi_transfernb((char*)_send_data, (char*)_read_data, (int)_len);
-      //_adc_data[(p * _steps) + s] = (uint16_t)(_read_data[0] << 8 | _read_data[1]);
+      //_adc_data[(p * STEPS) + s] = (uint16_t)(_read_data[0] << 8 | _read_data[1]);
       //_in[](double)(0xFFFF - _adc_data[i]) * (5.12f/65536.0f);
-      _in[ (p * _steps) + s ][REAL] = (double)((_read_data[0] << 8) | (_read_data[1])) * (5.12f/65536.0f);;
+      _in_v[p][s][REAL] = (double)((double)((0xFFFF - ((_read_data[0] << 8) | (_read_data[1]))) * (5.12f/65536.0f)) - 2.5f);
       //printf("%04x\n",  (uint16_t)(_read_data[0] << 8 | _read_data[1]));
 
       bcm2835_delayMicroseconds(_step_delay);
@@ -165,46 +186,94 @@ void run_daq() {
 
 void run_fft() {
 
-  int trim_samples = 64;
+  double total_fft_mag;
 
   //_in[](double)(0xFFFF - _adc_data[i]) * (5.12f/65536.0f);
 
-  fftw_execute(_p);
+  for (int i = 0; i < PERIODS; i++)
+    fftw_execute(_p_v[i]);
 
-  for (int i = 0; i < ADC_DATA_SIZE; i++) {
-    _fft_mag[i] = mag(_out[i][REAL], _out[i][IMAG]);
-    _fft_ang[i] = ang(_out[i][REAL], _out[i][IMAG]);
+  for (int s = 0; s < STEPS; s++, total_fft_mag = 0) {
+    for (int p = 0; p < PERIODS; p++) {
 
+      total_fft_mag += mag(_out_v[p][s][REAL], _out_v[p][s][IMAG]);
+      
+    }
+
+    //_prev_fft_mag[s] = _fft_mag[s];
+    _fft_mag[s] = _hamming[s] * (total_fft_mag / PERIODS);
   }
-
-  for ( int i = 0; i < trim_samples; i++ ) {
-    _fft_mag[i] = 0;
-    _fft_mag[ADC_DATA_SIZE - i - 1] = 0;
-  }
-
-  //int peak = 0;
-
-  //for (int i = 0; i < ADC_DATA_SIZE; i++)
-  //  if (_fft_mag[i] > peak) peak = i;
-
-  //printf("%f\t%f\n", _fft_ang[peak], _fft_mag[peak]);
 }
 
-void plot_adc() {
+void calibrate_freespace_fft(double freespace_weight) {
+
+
+  printf("Callibrating freespace\n");
+
+  int calibration_cycles = 10;
+  double total_fft_mag;
+
+
+  //for (int i = 0; i < 5; i++)
+  //  run_daq();
+
+
+  //_in[](double)(0xFFFF - _adc_data[i]) * (5.12f/65536.0f);
+  for (int c = 0; c < calibration_cycles; c++) {
+
+    printf("%d\n", calibration_cycles - c);
+
+    run_daq();
+
+    for (int i = 0; i < PERIODS; i++)
+      fftw_execute(_p_v[i]);
+
+    for (int s = 0; s < STEPS; s++, total_fft_mag = 0) {
+      for (int p = 0; p < PERIODS; p++) {
+
+        total_fft_mag += mag(_out_v[p][s][REAL], _out_v[p][s][IMAG]);
+        
+      }
+
+      //_prev_fft_mag[s] = _fft_mag[s];
+      _freespace_fft_mag[s] += _hamming[s] * (total_fft_mag / PERIODS);
+    }
+  }
+
+  for (int s = 0; s < STEPS; s++) {    
+    _freespace_fft_mag[s] /= calibration_cycles;
+    _freespace_fft_mag[s] *= freespace_weight;
+  }
+
+
+  printf("Done\n");
+
+}
+
+void plot_adc(bool overwrite) {
 
 	vector<double> xs, ys;
+
+  static int n = 0;
+  ostringstream filename;
+
+  filename << "screens/raw_voltage_plot" << n << ".png";
 
   //for (uint16_t data : adc_data)
   //  printf("%f\n", (0xFFFF - data) * (5.12f/65536.0f));
 
-  for (int i = 0; i < ADC_DATA_SIZE; i++) {
+  for (int p = 0; p < PERIODS; p++) {
+    for (int s = 0; s < STEPS; s++) {
 
-    //printf("%f\n", (0xFFFF - _adc_data[i]) * (5.12f/65536.0f));
+    //printf("%f\n", _in_v[p][s][REAL]);
     //printf("%04x\n", _adc_data[i]);
-    ys.push_back( (double)(0xFFFF - _adc_data[i]) * (5.12f/65536.0f) );
-    xs.push_back(i);
-
+    ys.push_back( (double)_in_v[p][s][REAL] );
+    //ys.push_back( (double)_in_v[1][s][REAL] );
+    xs.push_back( (p * STEPS) + s );
+    //xs.push_back(s);
+    }
   }
+
 
   //printf("%04x\n", step_incr);
 
@@ -214,20 +283,22 @@ void plot_adc() {
 	series->xs = &xs;
 	series->ys = &ys;
 
+	ScatterPlotSettings *settings = GetDefaultScatterPlotSettings();
+	settings->width = 800;
+	settings->height = 600;
+	settings->autoBoundaries = true;
+	settings->autoPadding = true;
+	settings->xMin = 0;
+	settings->xMax = STEPS;
+	settings->yMin = 2.5;
+	settings->yMax = 2.7;
+
+
 	//series->linearInterpolation = true;
 	//series->lineType = toVector(L"dashed");
 	//series->lineThickness = 2;
 	//series->color = GetGray(0.3);
 
-	ScatterPlotSettings *settings = GetDefaultScatterPlotSettings();
-	settings->width = 600;
-	settings->height = 400;
-	settings->autoBoundaries = false;
-	settings->autoPadding = true;
-	settings->xMin = 0;
-	settings->xMax = ADC_DATA_SIZE;
-	settings->yMin = 0;
-	settings->yMax = 6;
 	//settings->title = toVector(L"x^2 - 2");
 	//settings->xLabel = toVector(L"X axis");
 	//settings->yLabel = toVector(L"Y axis");
@@ -236,25 +307,32 @@ void plot_adc() {
 	DrawScatterPlotFromSettings(imageReference, settings);
 
 	vector<double> *pngdata = ConvertToPNG(imageReference->image);
-	WriteToFile(pngdata, "plot.png");
+	WriteToFile(pngdata, filename.str());
 	DeleteImage(imageReference->image);
+
+  if (!overwrite) n++;
 
 }
 
-void plot_fft() {
+void plot_fft(bool overwrite) {
 
 	vector<double> xs, ys;
+
+  static int n = 0;
+  ostringstream filename;
+
+  filename << "screens/fft_plot" << n << ".png";
 
   //for (uint16_t data : adc_data)
   //  printf("%f\n", (0xFFFF - data) * (5.12f/65536.0f));
 
-  for (int i = 0; i < ADC_DATA_SIZE; i++) {
+  for (int s = 0; s < STEPS; s++) {
 
     //printf("%f\n", (0xFFFF - _adc_data[i]) * (5.12f/65536.0f));
     //printf("%04x\n", _adc_data[i]);
     //ys.push_back( (double)(0xFFFF - _adc_data[i]) * (5.12f/65536.0f) );
-    ys.push_back((double)_fft_mag[i]);
-    xs.push_back(i);
+    ys.push_back((double)_fft_mag[s] - (double)(_freespace_fft_mag[s]));
+    xs.push_back(s);
 
   }
   
@@ -265,21 +343,21 @@ void plot_fft() {
 	ScatterPlotSeries *series = GetDefaultScatterPlotSeriesSettings();
 	series->xs = &xs;
 	series->ys = &ys;
-
-	//series->linearInterpolation = true;
-	//series->lineType = toVector(L"dashed");
-	//series->lineThickness = 2;
+	series->linearInterpolation = true;
 	//series->color = GetGray(0.3);
 
+	//series->lineType = toVector(L"dashed");
+	//series->lineThickness = 2;
+
 	ScatterPlotSettings *settings = GetDefaultScatterPlotSettings();
-	settings->width = 600;
-	settings->height = 400;
-	settings->autoBoundaries = true;
+	settings->width = 800;
+	settings->height = 600;
+	settings->autoBoundaries = false;
 	settings->autoPadding = true;
 	settings->xMin = 0;
-	settings->xMax = ADC_DATA_SIZE;
+	settings->xMax = STEPS;
 	settings->yMin = 0;
-	settings->yMax = 6;
+	settings->yMax = 0.3;
 	//settings->title = toVector(L"x^2 - 2");
 	//settings->xLabel = toVector(L"X axis");
 	//settings->yLabel = toVector(L"Y axis");
@@ -288,8 +366,10 @@ void plot_fft() {
 	DrawScatterPlotFromSettings(imageReference, settings);
 
 	vector<double> *pngdata = ConvertToPNG(imageReference->image);
-	WriteToFile(pngdata, "plot.png");
+	WriteToFile(pngdata, filename.str());
 	DeleteImage(imageReference->image);
+
+  if (!overwrite) n++;
 
 }
 
@@ -297,22 +377,34 @@ int main() {
 
 
   // zeroing imaginary nums for fft
-  //for (int i = 0; i < ADC_DATA_SIZE; i++) {
+  //for (int i = 0; i < STEPS; i++) {
   //  //_in[i][IMAG] = 0;
   //}
 
   if ( spi_init() ) return 1;
 
-  //for ( int i = 0; i < 5; i++ )
-  //  run_daq();
+  //calibrate_freespace_fft();
+  for (int i = 0; i < 5; i++)
+    run_daq();
 
-  for (int i = 0; i < 10; i++) {
+  //calibrate_freespace_fft(0.80f);
+
+  char c;
+
+  // control loop
+  while(1) {
+  //for ( int i = 0; i < 160; i++) {
+
+    if (cin.get() == 'q')
+      break;
 
     thread t_daq(run_daq);
-    thread t_fft(run_fft);
-    thread t_plot(plot_fft);
     t_daq.join();
+
+    thread t_fft(run_fft);
     t_fft.join();
+
+    thread t_plot(plot_fft, true);
     t_plot.join();
 
   }
@@ -325,6 +417,33 @@ int main() {
   //printf("g2 %d\n", bcm2835_gpio_lev(RPI_BPLUS_GPIO_J8_03));
   //printf("g1 %d\n", bcm2835_gpio_lev(RPI_BPLUS_GPIO_J8_05));
   //printf("g0 %d\n", bcm2835_gpio_lev(RPI_BPLUS_GPIO_J8_07));
+
+  //printf("03 %d\n", bcm2835_gpio_lev(RPI_BPLUS_GPIO_J8_03));
+  //printf("05 %d\n", bcm2835_gpio_lev(RPI_BPLUS_GPIO_J8_05));
+  //printf("07 %d\n", bcm2835_gpio_lev(RPI_BPLUS_GPIO_J8_07));
+  //printf("08 %d\n", bcm2835_gpio_lev(RPI_BPLUS_GPIO_J8_08));
+  //printf("10 %d\n", bcm2835_gpio_lev(RPI_BPLUS_GPIO_J8_10));
+  //printf("11 %d\n", bcm2835_gpio_lev(RPI_BPLUS_GPIO_J8_11));
+  //printf("12 %d\n", bcm2835_gpio_lev(RPI_BPLUS_GPIO_J8_12));
+  //printf("13 %d\n", bcm2835_gpio_lev(RPI_BPLUS_GPIO_J8_13));
+  //printf("15 %d\n", bcm2835_gpio_lev(RPI_BPLUS_GPIO_J8_15));
+  //printf("16 %d\n", bcm2835_gpio_lev(RPI_BPLUS_GPIO_J8_16));
+  //printf("18 %d\n", bcm2835_gpio_lev(RPI_BPLUS_GPIO_J8_18));
+  //printf("19 %d\n", bcm2835_gpio_lev(RPI_BPLUS_GPIO_J8_19));
+  //printf("21 %d\n", bcm2835_gpio_lev(RPI_BPLUS_GPIO_J8_21));
+  //printf("22 %d\n", bcm2835_gpio_lev(RPI_BPLUS_GPIO_J8_22));
+  //printf("23 %d\n", bcm2835_gpio_lev(RPI_BPLUS_GPIO_J8_23));
+  //printf("24 %d\n", bcm2835_gpio_lev(RPI_BPLUS_GPIO_J8_24));
+  //printf("26 %d\n", bcm2835_gpio_lev(RPI_BPLUS_GPIO_J8_26));
+  //printf("29 %d\n", bcm2835_gpio_lev(RPI_BPLUS_GPIO_J8_29));
+  //printf("31 %d\n", bcm2835_gpio_lev(RPI_BPLUS_GPIO_J8_31));
+  //printf("32 %d\n", bcm2835_gpio_lev(RPI_BPLUS_GPIO_J8_32));
+  //printf("33 %d\n", bcm2835_gpio_lev(RPI_BPLUS_GPIO_J8_33));
+  //printf("35 %d\n", bcm2835_gpio_lev(RPI_BPLUS_GPIO_J8_35));
+  //printf("36 %d\n", bcm2835_gpio_lev(RPI_BPLUS_GPIO_J8_36));
+  //printf("37 %d\n", bcm2835_gpio_lev(RPI_BPLUS_GPIO_J8_37));
+  //printf("38 %d\n", bcm2835_gpio_lev(RPI_BPLUS_GPIO_J8_38));
+  //printf("40 %d\n", bcm2835_gpio_lev(RPI_BPLUS_GPIO_J8_40));
 
   bcm2835_aux_spi_write(0x0000);
 
